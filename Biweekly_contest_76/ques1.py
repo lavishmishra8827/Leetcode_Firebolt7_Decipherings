c537d7560da265435d3b9b7890c0ad608c06773f
Lavish Mishra <lavishmishra8827@gmail.com>
2022-04-16 22:28:40 -0400

    Biweekly_contest_76_corrected

--- Biweekly_contest_76/ques1.py
+++ Biweekly_contest_76/ques1.py
@@ -1,22 +1,12 @@
 class Solution:
-    def minimumCost(self, cost):
-        cost.sort()
-        number=1
-        index=len(cost)-1
-        given_sum=0
-        while(index>=0):
-            if number%3==0:
-                index-=1
-                number+=1
-                continue
-            #print(index)
-            given_sum+=cost[index]
-            index-=1
-            number+=1
-        return given_sum
-if __name__=="__main__":
-    sol=Solution()
-    cost = [6,5,7,9,2,2]
-    cost = [5,5]
-    cost = [1,2,3]
-    print(sol.minimumCost(cost))
+    def findClosestNumber(self, nums: List[int]) -> int:
+        mindistance=100001
+        number=nums[0]
+        for i in nums:
+            distance_now=abs(i)
+            if distance_now<=mindistance:
+                if i<0 and distance_now==mindistance:
+                    continue
+                number=i
+                mindistance=distance_now
+        return number
